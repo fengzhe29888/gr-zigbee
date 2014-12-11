20289b0e876f5623f55da0973e1626ded923edd6
fengzhe29888 <fengzhe29888@gmali.com>
2014-12-11 02:09:21 -0500

    1. processing only one symbol very time in demodulator; 2. changed filter to vector dot product, 3:replaced vectors with pointers

--- lib/frame_length_detector_impl.cc
+++ lib/frame_length_detector_impl.cc
@@ -48,8 +48,7 @@ namespace gr {
 	d_symbol_table(symbol_table),
 	d_state(0),
 	d_remaining(0),
-	d_process(0),
-	d_filter(new filter::kernel::fir_filter_fff(d_Q,d_symbol_table))
+	d_process(0)
     {}
 
     /*
@@ -57,7 +56,6 @@ namespace gr {
      */
     frame_length_detector_impl::~frame_length_detector_impl()
     {
-	delete d_filter;
     }
 
     void
@@ -70,35 +68,23 @@ namespace gr {
     int
     frame_length_detector_impl::demodulator(const float input[])
     {
-	std::vector<float> d_pulse; //contains the matched filter tap for demodulation.
-	size_t align = volk_get_alignment();
-	float *d_matched_out = (float*) volk_malloc(2*sizeof(float),align);//decimated matched filter output
-	std::vector<float> max_out; //the max matched filter output among 16 pulses. 
-	std::vector<int> max_index;// the index of max matched filter output. 
-	for(int k = 0; k < 2; k++){
-	  max_out.push_back(-INFINITY);
-	  max_index.push_back(0);
-	}
+	float d_matched_out =0;
+	float max_out = -INFINITY; //the max matched filter output among 16 pulses. 
+        int max_index =0; 
 	//16-ary demodulation
-	for(int dj = 0; dj < 16; dj++){
-	  for(int dk = d_Q-1; dk >= 0; dk--){
-	    d_pulse.push_back(d_symbol_table[dj*d_Q+dk]);
-	  }//set matched filter taps.
-	  d_filter->set_taps(d_pulse);
-	  d_filter->filterNdec(d_matched_out, & d_byte_in_process[0], 2, d_Q);
-	  //printf("d_matched filter output are %f and %f:\n",d_matched_out[0],d_matched_out[1]);
-	  for(int dl=0; dl<2; dl++){
-	  if(d_matched_out[dl] > max_out[dl]){
-	    max_out[dl] = d_matched_out[dl];
-	    max_index[dl] = dj;
-	    }
-	  }
-	  d_pulse.clear();
-	}// for j=0~16; 
-	//printf("indeces with max output is %d and %d\n", max_index[0], max_index[1]);
-	int N = max_index[0]+max_index[1]*16;//compute the demodulated result here;
-	//printf("demodulated result is %d\n",N);
-	return N;
+        for(int dj = 0; dj < 16; dj++){
+          for(int dk = 0; dk < d_Q; dk++){
+            d_matched_out = d_matched_out + d_symbol_table[dj*d_Q+dk]*input[dk]; 
+          }
+	  //printf("d_matched_out is %d, %f\n", dj, d_matched_out);
+          if(d_matched_out> max_out){
+            max_out = d_matched_out;
+            max_index = dj;
+          }
+	  d_matched_out =0;
+        }// for j=0~16; 
+	printf("demodulated result is %f and %d\n",max_out, max_index);
+        return max_index;
     }
 
     int
@@ -117,90 +103,86 @@ namespace gr {
 	int l = 0; //state 0 index;
 	int j=0;
 	int ni=std::min(ninput_items[0],ninput_items[1]);
-
-	  if(d_state == 0){
-	    for(l = 0; l < ni; l++)
+	printf("In the beginning: ni is %d,%d,%d\n",ni,ninput_items[0],ninput_items[1]);
+        if(d_state == 0){
+          for(l = 0; l < ni; l++){
 	    //flags have not been detected yet
-	    if(in_flag[l] != 0){
-	      d_state = 1;
-	      printf("In state 0 consumed %d\n",l);
-	      consume_each(l);
-	      //printf("after state 0 consume: in_data is %f,in_flag is %d\n",in_data[l],in_flag[l]);
+            if(in_flag[l] != 0){
+              d_state = 1;
+	      printf("In state 0 consumed %d\n",l+1);
+              consume_each(l+1);
+	      printf("after state 0 consume: in_data is %f,in_flag is %d\n",in_data[l+1],in_flag[l+1]);
 	      //printf("ni is %d,%d,%d\n",ni,ninput_items[0],ninput_items[1]);
 	      return 0;
 	    }
+          }
 	      consume_each(ni);
 	      return 0;
 	   }
-	   else if(d_state==1){
-	     for(j = 0; j < 2*d_Q; j++){
-		 d_byte_in_process.push_back(in_data[j]);
-	        //printf("in_data are %f\n",in_data[j]);
-	      }//for loop
-	      //printf("the current flag is %d\n",in_flag[0]);
-	      d_N = demodulator(& d_byte_in_process[0]);
-	      if(d_N > 127){// Since 7 bits is used for frame length, the frame length is wrong if it's greater than 127.
-		d_state = 0;	        //so we enter state 0 to search the next flags. 
-                 d_byte_in_process.clear();
-		consume_each(1); // the input pointer is updated by 1.
-		return 0;
-		printf("detected frame length is out of range, research a flag");		
-	       }
-	      else{
-	        d_remaining = 2*d_N*d_Q; // when d_N is smaller than 127, the frame length detected might form a packet. the remaining bits to be processed in state 2.
-	        //printf("d_N %d and d_remaining %d\n",2*d_N*d_Q, d_remaining);
-	        printf("frame length is %d\n",d_N);
-                 d_byte_in_process.clear();
-	        consume_each(2*d_Q);
-	        //printf("end of state 1 input pointer updated %d\n",j);
-	        //printf("after state 1, the current input data and flags are: in_data is %f,in_flag is %d\n",in_data[j],in_flag[j]);
-	        d_state = 2;
-	        return 0;
-	      }
-	   }//else d_state ==1
+        else if(d_state==1){
+          int d_length[2];
+          for(int jj =0; jj<2; jj++){
+            d_length[jj] = demodulator(&in_data[jj*d_Q]);
+          }
+          d_N = d_length[0]+d_length[1]*16;
+          if(d_N > 127){// Since 7 bits is used for frame length, the frame length is wrong if it's greater than 127.
+            d_state = 0;	        //so we enter state 0 to search the next flags. 
+            consume_each(1); // the input pointer is updated by 1.
+            return 0;		//printf("detected frame length is out of range, research a flag");		
+          }
+          else{
+            d_remaining = 2*d_Q*d_N; // when d_N is smaller than 127, the frame length detected might form a packet. the remaining bits to be processed in state 2.
+            //printf("d_N %d and d_remaining %d\n",2*d_N*d_Q, d_remaining);
+            printf("frame length is %d\n",d_N);
+            consume_each(2*d_Q);
+            //printf("end of state 1 input pointer updated %d\n",j);
+            printf("after state 1, the current input data and flags are: in_data is %f,in_flag is %d\n",in_data[2*d_Q],in_flag[2*d_Q]);
+            d_state = 2;
+            return 0;
+          }
+        }//else d_state ==1
 
-	   else if(d_state == 2){
+        else if(d_state == 2){
 	     //printf("d_remaining is %d\n",d_remaining);
-	     if(d_remaining > ni){ // if d_remaining is greater than ni, we only process ni bits with the current buffer.
-		printf("ni is %d, d_remaining is %d\n",ni, d_remaining);
-		d_process = ni; 
-		d_remaining = d_remaining -d_process;
-		printf("d_remaining lager than ni d_process: %d and d_remaining:%d\n", d_process, d_remaining);
-	      }
-	     else{
-		printf("ni is %d, and d_remaining is %d\n",ni, d_remaining);
-		d_process = d_remaining;
-		d_remaining = 0;
-		printf("d_remaining smaller than ni d_process: %d and d_remaining:%d\n", d_process, d_remaining);
-	     }
+          if(d_remaining > ni){ // if d_remaining is greater than ni, we only process ni bits with the current buffer.
+            printf("ni is %d, d_remaining is %d\n",ni, d_remaining);
+            d_process = ni; 
+            d_remaining = d_remaining -d_process;
+            printf("d_remaining lager than ni d_process: %d and d_remaining:%d\n", d_process, d_remaining);
+          }
+          else{
+            printf("ni is %d, and d_remaining is %d\n",ni, d_remaining);
+            d_process = d_remaining;
+            d_remaining = 0;
+            //printf("d_remaining smaller than ni d_process: %d and d_remaining:%d\n", d_process, d_remaining);
+          }
 
+          int d_byte_out[2];
+          for(j =0 ;j < d_process; j++){
+            if(j%(2*d_Q)==0){
+              no = j/(2*d_Q);
+              d_byte_out[0] = demodulator(&in_data[j]);
+              d_byte_out[1] = demodulator(&in_data[j+d_Q]);
+              d_result.push_back(d_byte_out[0]+ 16*d_byte_out[1]);
+              printf("the %dth result is %d\n", no+1,d_result[no]);
+              out[no]=d_result[no];
+            }
+          }
 
-	     for(j =0 ;j < d_process; j++){
-		 d_byte_in_process.push_back(in_data[j]);
-		if( d_byte_in_process.size()%(2*d_Q)==0){
-		  no=j/(2*d_Q);
-		  //printf("the last data in 128 to be demod is %f\n",in_data[j]);
-		  d_result.push_back(demodulator(& d_byte_in_process[0]));
-		   d_byte_in_process.clear();
-		  printf("the %dth result is %d\n", no+1,d_result[no]);
-		  out[no]=d_result[no];
-		}
-	     }
-
-	     if(d_remaining ==0){
-       	       d_state = 0;
-	     }
-	     else{
-	       d_state = 2;
-	       printf("continuing state =2, d_remaining is %d, processed is %d\n",d_remaining, d_process);
-	     }
-	     d_result.clear();
+          if(d_remaining ==0){
+            d_state = 0;
+          }
+          else{
+            d_state = 2;
+	       //printf("continuing state =2, d_remaining is %d, processed is %d\n",d_remaining, d_process);
+          }
+          d_result.clear();
 	     //printf("the input pointer update after state 2 is %d\n",j);
-	     consume_each(d_process);
-	     //printf("In state 2 it consumed %d samples: the current in_data is %f,in_flag is %d\n",d_process,in_data[j],in_flag[j]);
-	     return no+1;
-	    }//d_state =2*/
-    }
+          consume_each(d_process);
+	     printf("In state 2 it consumed %d samples: the current in_data is %f,in_flag is %d\n",d_process,in_data[no],in_flag[no]);
+          return no+1;
+        }//d_state =2*/
+    }//work
 
   } /* namespace zigbee */
 } /* namespace gr */
